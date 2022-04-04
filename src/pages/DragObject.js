b50efab8c1bb6d944db8318c41291be919fe5f98
bojne <bojne.john@gmail.com>
2022-04-04 23:39:53 +0200

    make multiple drag, counter controlled state and background image css

--- src/pages/DragObject.js
+++ src/pages/DragObject.js
@@ -1,22 +1,65 @@
-import React from "react";
-import DraggableObject from "../Components/Dragaable";
+import React, { useState } from "react";
 import Draggable from "react-draggable";
-import RotatableEmoji from "../Components/RotateText";
 import styled from "styled-components";
-
+import Counter from "../Components/Counter";
 const Container = styled.div`
   width: 100%;
   margin-top: 2rem;
 `;
 
+const Image = styled.div`
+  background-image: url("https://github.com/Bojne/learn-react/blob/master/public/logo.png?raw=true");
+  background-repeat: no-repeat;
+  background-size: contain;
+  color: gray;
+  height: ${(props) => props.size}rem;
+  width: ${(props) => props.size}rem;
+  border: 4px dotted rgba(255, 255, 255, 0);
+  &:active {
+    border: 4px dotted blue;
+  }
+`;
+
+const DraggableEmoji = ({ size, setStatus }) => {
+  const startDragging = () => setStatus("Dragging!");
+  const stopDragging = () => setStatus("Done :)");
+
+  return (
+    <Draggable
+      onStart={startDragging}
+      onDrag={startDragging}
+      onStop={stopDragging}
+    >
+      <Image size={size}></Image>
+    </Draggable>
+  );
+};
+
+const FlexContiner = styled.div`
+  display: flex;
+`;
+
 const DragObject = () => {
+  const defSize = 5;
+  const [size, setSize] = useState(defSize);
+  const [status, setStatus] = useState("Sitting...");
+  const balls = Array.from(Array(10).keys());
   return (
     <Container>
-      <Draggable>
-        <div>
-          <img src="https://github.com/Bojne/learn-react/blob/master/public/logo.png?raw=true"></img>
-        </div>
-      </Draggable>
+      <h3>Status: {status}</h3>
+      <FlexContiner>
+        <h4>Size: </h4>
+        <Counter
+          value={size}
+          setValue={setSize}
+          defaultValue={defSize}
+        ></Counter>
+      </FlexContiner>
+      <FlexContiner>
+        {balls.map((b) => (
+          <DraggableEmoji size={size} setStatus={setStatus}></DraggableEmoji>
+        ))}
+      </FlexContiner>
     </Container>
   );
 };
